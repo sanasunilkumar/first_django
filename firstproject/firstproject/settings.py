e24ea612af6d244c9eb6c860bb7f6347e8ab34d1
SANA SUNIL KUMAR <102207809+sanasunilkumar@users.noreply.github.com>
2023-10-16 18:20:17 +0530

    Update settings.py

--- firstproject/firstproject/settings.py
+++ firstproject/firstproject/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ow)oc08bh0uw%d%=*(#*jz22=(&q_s*3pz1*1lp*g(%%2x2341'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
